9ff16c1cd47fa5a695c31905b38fbc56f40fbc45
Alejandro <jandro.bernardez.araujo@gmail.com>
2020-01-27 23:20:29 +0100

    Add - Añadido el nombre del archivo en el input de búsqueda.

--- Imperator Stats/wwwroot/js/site.js
+++ Imperator Stats/wwwroot/js/site.js
@@ -2,3 +2,10 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(function () {
+    /* Función para obtener el nombre del archivo seleccionado */
+    $(".custom-file-input").on("change", function () {
+        var fileName = $(this).val().split("\\").pop();
+        $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
+    });
+});
